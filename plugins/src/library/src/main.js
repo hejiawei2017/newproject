f0c3db79dd736e148bd3e145046b7256e7104af9
hejiawei2017 <499200621@qq.com>
2021-03-15 20:44:42 +0800

    提交修改

--- plugins/src/library/src/main.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') { // 通过环境变量来决定入口文件
-    module.exports = require('../dist/suyuanui.min.js')
-} else {
-    module.exports = require('../dist/suyuanui.js')
-}
--- plugins/src/library/src/packages/Table/index.js
+++
@@ -1,8 +0,0 @@
-import Table from './src/main.vue';
-
-/* istanbul ignore next */
-Table.install = function (Vue) {
-  Vue.component(Table.name, Table);
-};
-
-export default Table;
